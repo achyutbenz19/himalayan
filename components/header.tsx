c097a0161e01268f378bdd436a8844faeaf61a2c
achyutbenz19 <achyut.benz@gmail.com>
2024-08-13 23:49:22 -0500

    css

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,11 @@ import Logo from "./logo";
 
 const Header = () => {
   return (
-    <header className="flex p-4 h-full items-center justify-center lg:justify-between flex-row w-full">
+    <header className="flex p-4 h-full items-center justify-center lg:justify-between flex-col lg:flex-row w-full">
       <div>
         <Logo height={150} width={150} />
       </div>
+      <div className="flex flex-row p-3">hi</div>
     </header>
   );
 };
