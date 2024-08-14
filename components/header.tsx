f6937d7fef448295cdbe708ea571d67e0ec750dc
achyutbenz19 <achyut.benz@gmail.com>
2024-08-14 00:38:19 -0500

    header

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import Logo from "./logo";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
 const Header = () => {
   return (
@@ -10,7 +17,45 @@ const Header = () => {
         <Logo height={150} width={150} />
       </div>
       <div className="flex lg:justify-end tracking-tight justify-center flex-row px-3 pt-2 w-full">
-        <Button className="text-xl font-thin" variant="link">
+        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="space-x-2 flex h-full items-center">
+              <span>Menu</span>
+              <ChevronDown className="flex-shrink-0 h-5 w-5" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+                Niles location
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+                Chicago downtown
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </Button>
+        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="space-x-2 flex h-full items-center">
+              <span>Order online</span>
+              <ChevronDown className="flex-shrink-0 h-5 w-5" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+                Niles location
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+                Chicago downtown
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </Button>
+        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+          <Link href="/deals">Deals</Link>
+        </Button>
+        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+          <Link href="/rewards">Rewards</Link>
+        </Button>
+        <Button className="lg:text-2xl text-xl font-thin" variant="link">
           <Link href="/blog">Blog</Link>
         </Button>
       </div>
