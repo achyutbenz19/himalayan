49bdc4bc7441ee2e08be484d34fbe97b3cda378b
achyutbenz19 <achyut.benz@gmail.com>
2024-08-16 14:55:42 -0500

    css

--- components/header.tsx
+++ components/header.tsx
@@ -12,13 +12,13 @@ import { ChevronDown } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="flex h-full items-center justify-center lg:justify-between flex-col lg:flex-row w-full">
+    <header className="flex h-full items-center justify-center md:justify-between flex-col md:flex-row w-full">
       <div className="pt-1">
         <Logo height={150} width={150} />
       </div>
-      <div className="flex lg:justify-end tracking-tight justify-center flex-row px-3 pt-2 w-full">
+      <div className="flex md:justify-end tracking-tight justify-center flex-row px-3 pt-2 w-full">
         <Button
-          className="lg:text-2xl bg-transparent text-xl font-thin"
+          className="md:text-2xl bg-transparent text-xl font-thin"
           variant="outline"
         >
           <DropdownMenu>
@@ -27,42 +27,42 @@ const Header = () => {
               <ChevronDown className="flex-shrink-0 h-5 w-5" />
             </DropdownMenuTrigger>
             <DropdownMenuContent sideOffset={20}>
-              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+              <DropdownMenuItem className="cursor-pointer md:text-lg">
                 Niles location
               </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+              <DropdownMenuItem className="cursor-pointer md:text-lg">
                 Chicago downtown
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </Button>
-        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+        <Button className="md:text-2xl text-xl font-thin" variant="link">
           <DropdownMenu>
             <DropdownMenuTrigger className="space-x-2 flex h-full items-center">
               <span>Order online</span>
               <ChevronDown className="flex-shrink-0 h-5 w-5" />
             </DropdownMenuTrigger>
             <DropdownMenuContent sideOffset={10}>
-              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+              <DropdownMenuItem className="cursor-pointer md:text-lg">
                 Niles location
               </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer lg:text-lg">
+              <DropdownMenuItem className="cursor-pointer md:text-lg">
                 Chicago downtown
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </Button>
-        <Button className="lg:text-2xl text-xl font-thin" variant="link">
+        <Button className="md:text-2xl text-xl font-thin" variant="link">
           <Link href="/deals">Deals</Link>
         </Button>
         <Button
-          className="lg:text-2xl text-xl font-thin md:flex hidden"
+          className="md:text-2xl text-xl font-thin md:flex hidden"
           variant="link"
         >
           <Link href="/rewards">Rewards</Link>
         </Button>
         <Button
-          className="lg:text-2xl text-xl font-thin md:flex hidden"
+          className="md:text-2xl text-xl font-thin md:flex hidden"
           variant="link"
         >
           <Link href="/blog">Blog</Link>
