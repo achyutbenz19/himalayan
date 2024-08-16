f08c766f007ff60a7b94afeb870552bbe2b87502
achyutbenz19 <achyut.benz@gmail.com>
2024-08-16 15:58:43 -0500

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero";
 import Locations from "@/components/locations";
+import Menu from "@/components/menu";
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col">
       <Hero />
       <Locations />
+      <Menu />
     </main>
   );
 }
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import LocationCard from "./location-card";
 
-const Locations = () => {
+const Menu = () => {
   return (
     <div className="lg:px-4 mt-6 w-full lg:mt-10">
       <span className="text-2xl tracking-tighter sm:text-4xl md:text-4xl xl:text-[3rem] 2xl:text-[3.4rem]">
-        Our locations
+        Our menu
       </span>
-      <div className="flex gap-4 mt-4 flex-col md:flex-row">
-        <LocationCard />
-        <LocationCard />
-      </div>
     </div>
   );
 };
 
-export default Locations;
+export default Menu;
