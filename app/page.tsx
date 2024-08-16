18b80c38583fbb7e82a97538b5bf858e4c8657f9
achyutbenz19 <achyut.benz@gmail.com>
2024-08-16 09:40:08 -0500

    locations beta

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import Locations from "@/components/locations";
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col">
       <Hero />
+      <Locations />
     </main>
   );
 }
