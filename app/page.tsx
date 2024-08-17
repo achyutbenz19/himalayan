38b48cff591d5761e5cb016efe7dee2047a56ba5
achyutbenz19 <achyut.benz@gmail.com>
2024-08-17 09:18:31 -0500

    testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Locations from "@/components/locations";
 import Menu from "@/components/menu";
+import { Testimonials } from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <Locations />
         <Menu />
+        <Testimonials />
       </div>
       <Footer />
     </main>
---
+++ components/testimonials.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { InfiniteMovingCards } from "./infinite-moving-cards";
+
+export function Testimonials() {
+  return (
+    <div className="lg:px-8 mt-10 w-full lg:mt-10">
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
+    </div>
+  );
+}
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+    avatar: "https://i.pravatar.cc/150?img=1",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+    avatar: "https://i.pravatar.cc/150?img=2",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+    avatar: "https://i.pravatar.cc/150?img=3",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+    avatar: "https://i.pravatar.cc/150?img=4",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+    avatar: "https://i.pravatar.cc/150?img=5",
+  },
+];
