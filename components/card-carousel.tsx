9d87768f8f333454336e9c36b89ef0c7dce2ab1e
achyutbenz19 <achyut.benz@gmail.com>
2024-08-17 23:44:55 -0500

    css

--- components/card-carousel.tsx
+++ components/card-carousel.tsx
@@ -100,7 +100,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
 
           <div
             className={cn(
-              "flex flex-row justify-start gap-4",
+              "flex flex-row justify-start gap-2",
               "max-w-7xl mx-auto", // remove max-w-4xl if you want the carousel to span the full width of its container
             )}
           >
