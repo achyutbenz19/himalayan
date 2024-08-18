bacecec1e9c2fe530bf2784eab648af8b77a7217
achyutbenz19 <achyut.benz@gmail.com>
2024-08-17 23:40:02 -0500

    css

--- components/custom-card.tsx
+++ components/custom-card.tsx
@@ -14,7 +14,7 @@ export function CustomCard({
       <div
         className={cn(
           className,
-          "cursor-pointer overflow-hidden relative card rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+          "cursor-pointer overflow-hidden relative card rounded-3xl shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
           "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover",
         )}
       >
