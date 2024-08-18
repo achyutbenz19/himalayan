8bbbc72a98cd40a925edb79dbd2445e6d4c44eaa
achyutbenz19 <achyut.benz@gmail.com>
2024-08-17 23:36:04 -0500

    css

--- components/card-carousel.tsx
+++ components/card-carousel.tsx
@@ -128,7 +128,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
             ))}
           </div>
         </div>
-        <div className="flex justify-end gap-2 mt-8 mr-10">
+        <div className="flex justify-end gap-2 mt-4">
           <button
             className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
             onClick={scrollLeft}
